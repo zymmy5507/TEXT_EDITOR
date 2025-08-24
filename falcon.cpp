d8332222aa74f85b527100a699ff628aec9146ed
zymmy5507 <darkvarun5507@gmail.com>
2025-08-24 22:37:49 +0530

    Added status bar at the bottom of the editor

--- falcon.cpp
+++ falcon.cpp
@@ -2,18 +2,35 @@
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Menu_Bar.H>
 #include <FL/Fl_Text_Editor.H>
+#include <FL/Fl_Box.H>  // Required for status bar
+
+// Global variables
+Fl_Double_Window *main_win = nullptr;
+Fl_Text_Buffer *textbuf = nullptr;
+Fl_Box *status_bar = nullptr;
 
 int main(int argc, char **argv) {
-    main_win = new EditorWindow(1000, 700, "Simple Editor");
+    // Create the main window
+    main_win = new Fl_Double_Window(1000, 700, "Simple Editor");
     main_win->begin();
-    
+
+    // Menu bar
     Fl_Menu_Bar *menubar = new Fl_Menu_Bar(0, 0, 1000, 25);
-    // Add basic menu structure with placeholder callbacks
-    
-    Fl_Text_Editor *editor = new Fl_Text_Editor(0, 25, 1000, 675);
+    // i will work later
+
+    // Text editor area
+    Fl_Text_Editor *editor = new Fl_Text_Editor(0, 25, 1000, 650);
     textbuf = new Fl_Text_Buffer;
     editor->buffer(textbuf);
-    
+
+    // Status bar
+    status_bar = new Fl_Box(0, 675, 1000, 25, "Ready");
+    status_bar->box(FL_DOWN_BOX);
+    status_bar->labelfont(FL_HELVETICA);
+    status_bar->labelsize(12);
+    status_bar->align(FL_ALIGN_LEFT | FL_ALIGN_INSIDE);
+
+    main_win->resizable(editor);
     main_win->end();
     main_win->show(argc, argv);
     return Fl::run();
